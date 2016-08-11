fb68fb16ec7d5e00c3d39a3097caba44c8015d61
gjincai <1262868349@qq.com>
2016-08-11 23:40:30 +0800

    ci

--- demo1/index.js
+++ demo1/index.js
@@ -1,8 +1,3 @@
-// ReactDOM.render(
-//   <h1>Hello, world!</h1>,
-//   document.getElementById('jc')
-// );
-
 var Hello = React.createClass({
   render:function(){
     // return <span style={{color:'red'}}>hello {this.props.name}</span>;
